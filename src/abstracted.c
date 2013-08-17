f67baeeb8bc2b6a3f6e4cf0f876452d780a8d7f8
Kevin Weber <mrbetis@gmail.com>
2013-08-16 22:37:01 -0700

    Release
    Removed custom pixel-array font experiments
    Cleaned code
    Added setup to use digits in future updates
    Added menu icon

--- src/abstracted.c
+++ src/abstracted.c
@@ -7,19 +7,20 @@
 #include "pebble_os.h"
 #include "pebble_app.h"
 #include "pebble_fonts.h"
+#include "resource_ids.auto.h"
+
 
 #define MY_UUID { 0x74, 0x30, 0xBC, 0xF6, 0x1C, 0xED, 0x48, 0xA7, 0x8E, 0x23, 0xD4, 0x96, 0x79, 0x52, 0xC1, 0xF8 }
 PBL_APP_INFO(MY_UUID,
              "Abstracted", "Kevin Weber",
-             1, 2, /* App version */
-             DEFAULT_MENU_ICON, /* Change this for watchface icon?? */
+             1, 0, /* App version */
+              RESOURCE_ID_IMAGE_MENU_ICON, /* Change this for watchface icon?? */
              APP_INFO_WATCH_FACE);
 
 Window window;
 Layer abst;
-TextLayer cornerDate;
+Layer date;
 
-//PblTm Ctime;
 
 GPath cornerPath, pmPath;
 static const GPathInfo CORNER_BLOCK_PATH = {
@@ -34,25 +35,23 @@ static const GPathInfo PM_BLOCK_PATH = {
 
 
 
-GPoint loc( unsigned short x, unsigned short y ){
-	return GPoint( x, y );
-}
+// Adjustable settings
+#define SHOW_DATE true
+#define HOUR_MARKINGS true
+#define DOUBLE_MINUTES false
+#define HIGHLIGHT_MINUTES false
+#define HIGHLIGHT_INTERVAL 10
 
+// Constants, best to not edit
 #define HOUR_BAR_HEIGHT 7
 #define MINUTE_SPACING 4
 #define PM_SIZE 72
 
-#define DOUBLE_MINUTES false
-#define HIGHLIGHT_MINUTES false
-#define HIGHLIGHT_INTERVAL 10
-	
 void draw_face( AppContextRef ctx )
 {
 	PblTm t;
 	get_time( &t );
 	
-	// Declare/define date string to write to later
-	static char date_text[] = "00";
 	
 	// Declare Time Variables
 	unsigned short count_m = t.tm_min;
@@ -62,12 +61,13 @@ void draw_face( AppContextRef ctx )
 	// Set Drawing Colors
 	graphics_context_set_fill_color( ctx, GColorBlack );
 	graphics_context_set_stroke_color( ctx, GColorBlack );
-	graphics_context_set_text_color( ctx, GColorWhite );
+	graphics_context_set_text_color( ctx, GColorBlack );
 	
 	
 	// Draw PM Shade
 	if( isPM )
 	{
+		// Basically a bunch of lines spaced 1px apart to make a grey shade
 		for( unsigned short p=1; p<PM_SIZE+1; p++ ){
 			graphics_draw_line( ctx, GPoint( p*2, 168 ), GPoint( 144, 24 + p*2 ) );
 		}
@@ -75,57 +75,56 @@ void draw_face( AppContextRef ctx )
 	
 	// Draw Hour Rectangles
 	//count_h = 11; // Debug test value for full amount of hours
+	graphics_context_set_stroke_color( ctx, GColorWhite );
+	
 	for( unsigned short h=0; h<count_h; h++)
 	{
 		graphics_fill_rect( ctx, 
 					   GRect( 0, h*HOUR_BAR_HEIGHT*2 + HOUR_BAR_HEIGHT, 144, HOUR_BAR_HEIGHT+1 ), 
 					   0, GCornerNone );
 		
-		
 		/* Ghetto rectangles (pancaked lines); very slow
 		for( unsigned short i=0; i<=HOUR_BAR_HEIGHT; i++ ){
 			graphics_draw_line( ctx, loc( 0, HOUR_BAR_HEIGHT + (h*HOUR_BAR_HEIGHT*2) + i ), loc( 144, HOUR_BAR_HEIGHT + (h*HOUR_BAR_HEIGHT*2) + i ) );
 		}*/
+		
+		if( HOUR_MARKINGS )
+		{
+			for( int m=0; m<h; m++ )
+			{
+				// Draw Hour Markings at the left side of each hour bar
+			}
+		}
 	}
 	
 	// Draw Minute Lines
+	graphics_context_set_stroke_color( ctx, GColorBlack );
+	
 	for( unsigned short m=0; m<count_m; m++ )
 	{
 		unsigned short y_pos_left = 168 - MINUTE_SPACING*(m+1);
-		graphics_draw_line( ctx, loc( 0, y_pos_left ), loc( 144, y_pos_left+144 ) );
-		if( (m+1)%HIGHLIGHT_INTERVAL == 0 && HIGHLIGHT_MINUTES ) graphics_draw_line( ctx, loc( 0, y_pos_left-1 ), loc( 144, y_pos_left+143 ) );
 		
-		if( DOUBLE_MINUTES ) graphics_draw_line( ctx, loc( 0, y_pos_left-1 ), loc( 144, y_pos_left+143 ) );
+		graphics_draw_line( ctx, GPoint( 0, y_pos_left ), GPoint( 144, y_pos_left+144 ) );
+		
+		if( (m+1)%HIGHLIGHT_INTERVAL == 0 && HIGHLIGHT_MINUTES )
+		{
+			graphics_draw_line( ctx, GPoint( 0, y_pos_left-1 ), GPoint( 144, y_pos_left+143 ) );
+		}
+		
+		if( DOUBLE_MINUTES ) graphics_draw_line( ctx, GPoint( 0, y_pos_left-1 ), GPoint( 144, y_pos_left+143 ) );
 	}
 	
 	// Draw Static Block
 	gpath_draw_filled( ctx, &cornerPath );
-	
-	// Draw Date Text
-	
-	// "%a %d"
-	string_format_time( date_text, sizeof(date_text), "%a %d", &t );
-	//text_layer_set_text( &cornerDate, date_text );
-	/*graphics_text_draw( ctx, 
-				   date_text, 
-				   FONT_KEY_ROBOTO_CONDENSED_21, 
-				   GRect(108, 0, 144, 36),
-				   GTextOverflowModeWordWrap,
-				   GTextAlignmentCenter, 
-				   NULL
-				 );*/
 }
 
-void date_update_callback( Layer *me, GContext* ctx )
+void draw_date( AppContextRef ctx )
 {
-	(void)me;
-	// Draw Date Text
-	
-	// "%a %d"
-	//string_format_time( date_text, sizeof(date_text), "%a %d", &t );
-	//text_layer_set_text( &cornerDate, date_text );
+	if( SHOW_DATE )
+	{
+		//Draw Date
+	}
 }
-
 
 void abst_update_callback(Layer *me, GContext* ctx )
 {
@@ -133,37 +132,38 @@ void abst_update_callback(Layer *me, GContext* ctx )
 	draw_face( ctx );
 }
 
+void date_update_callback( Layer *me, GContext* ctx )
+{
+	(void)me;
+	draw_date( ctx );
+}
+
 
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
   (void)t;
   (void)ctx;
   layer_mark_dirty( &abst );
-  //layer_mark_dirty( &cornerDate );
-  //display_time(t->tick_time, ctx);
 }
 
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
 
+  resource_init_current_app(&APP_RESOURCES);
+
   window_init(&window, "Abstracted");
   window_stack_push(&window, true);
   window_set_background_color(&window, GColorWhite);
 
   layer_init(&abst, window.layer.frame);
+  layer_init(&date, GRect( 0, 108, 36, 36 ) );
   abst.update_proc = abst_update_callback;
+  date.update_proc = date_update_callback;
 
   gpath_init( &cornerPath, &CORNER_BLOCK_PATH );
 
-  text_layer_init(&cornerDate, GRect(108, 0, 144, 36));
-  //cornerDate.update_proc = date_update_callback;
-  text_layer_set_text_alignment(&cornerDate, GTextAlignmentCenter);
-  text_layer_set_background_color( &cornerDate, GColorBlack );
-  text_layer_set_text_color( &cornerDate, GColorWhite );
-  text_layer_set_font(&cornerDate, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-
   layer_add_child(&window.layer, &abst);
-  layer_add_child(&window.layer, &cornerDate.layer);
+  layer_add_child(&window.layer, &date);
 
   handle_minute_tick(ctx, NULL);
 }
